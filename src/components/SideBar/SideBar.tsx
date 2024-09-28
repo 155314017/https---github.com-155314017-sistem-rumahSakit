e6b7001a757d9f626b978c4f6ebd0a5403c3bcee
Dito <dhito.pb97@gmail.com>
2024-09-28 19:39:30 +0700

    fixing sidebar new design

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,78 +1,110 @@
-import {
-  Box,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  Typography,
-} from "@mui/material";
+import { Box, Stack, IconButton, Tooltip } from "@mui/material";
 
 // icon
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import PeopleIcon from "@mui/icons-material/People";
 import LocalHospitalIcon from "@mui/icons-material/LocalHospital";
 import MedicalServicesIcon from "@mui/icons-material/MedicalServices";
-import MeetingRoomIcon from "@mui/icons-material/MeetingRoom";
-import MonitorHeartIcon from '@mui/icons-material/MonitorHeart';
+import HotelIcon from "@mui/icons-material/Hotel";
 
-const iconMapping: { [key: string]: JSX.Element } = {
-  Dashboard: <DashboardIcon />,
-  Ruangan: <MeetingRoomIcon />,
-  Pegawai: <PeopleIcon />,
-  Fasilitas: <MonitorHeartIcon />,
-  Dokter: <MedicalServicesIcon />,
-  Poliklinik: <LocalHospitalIcon />,
-};
+// image
+import Logo from "../../assets/img/Logo - St carolus.png";
+
 export default function SideBar() {
   return (
     <Box>
       <Box
         position={"fixed"}
         component="nav"
-        width={"358px"}
-        height={"100%"}
         sx={{
           top: 0,
           left: 0,
-          borderRight: "1px solid #A8A8BD",
-          backgroundColor: "#fff",
+          p: 3,
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
         }}
       >
-        <Box p={5}>
-          <Typography fontWeight={"700"} fontSize={"25px"} color="#8F85F3">
-            Hospital System
-          </Typography>
+        <Box>
+          <img src={Logo} alt="logo" style={{ width: "100px" }} />
         </Box>
-        <List sx={{ p: 3, mt: -3 }}>
-          {[
-            "Dashboard",
-            "Ruangan",
-            "Pegawai",
-            "Fasilitas",
-            "Dokter",
-            "Poliklinik",
-          ].map((text) => (
-            <ListItem key={text}>
-              <ListItemButton
-                sx={{
-                  borderRadius: "8px",
-                  color: "#8F85F3",
-                  ":hover": {
-                    backgroundColor: "#8F85F3",
-                    color: "#fff",
-                    "& .MuiListItemIcon-root": { color: "#fff" },
-                  },
-                }}
-              >
-                <ListItemIcon sx={{ color: "#8F85F3" }}>
-                  {iconMapping[text]}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
+
+        <Stack mt={3} spacing={3}>
+          {/* dashboard */}
+          <Tooltip title="Dashboard" placement="right" arrow>
+            <IconButton
+              size="large"
+              sx={{
+                color: "#8F85F3",
+                bgcolor: "#fff",
+                width: "50px",
+                height: "50px",
+              }}
+            >
+              <DashboardIcon />
+            </IconButton>
+          </Tooltip>
+
+          {/* user-management */}
+          <Tooltip title="User Management" placement="right" arrow>
+            <IconButton
+              size="large"
+              sx={{
+                color: "#8F85F3",
+                bgcolor: "#fff",
+                width: "50px",
+                height: "50px",
+              }}
+            >
+              <PeopleIcon />
+            </IconButton>
+          </Tooltip>
+
+          {/* health-manager */}
+          <Tooltip title="Health Manager" placement="right" arrow>
+            <IconButton
+              size="large"
+              sx={{
+                color: "#8F85F3",
+                bgcolor: "#fff",
+                width: "50px",
+                height: "50px",
+              }}
+            >
+              <MedicalServicesIcon />
+            </IconButton>
+          </Tooltip>
+
+          {/* facility */}
+          <Tooltip title="Facility" placement="right" arrow>
+            <IconButton
+              size="large"
+              sx={{
+                color: "#8F85F3",
+                bgcolor: "#fff",
+                width: "50px",
+                height: "50px",
+              }}
+            >
+              <HotelIcon />
+            </IconButton>
+          </Tooltip>
+
+          {/* clinic */}
+          <Tooltip title="Clinic" placement="right" arrow>
+            <IconButton
+              size="large"
+              sx={{
+                color: "#8F85F3",
+                bgcolor: "#fff",
+                width: "50px",
+                height: "50px",
+              }}
+            >
+              <LocalHospitalIcon />
+            </IconButton>
+          </Tooltip>
+        </Stack>
       </Box>
     </Box>
   );
