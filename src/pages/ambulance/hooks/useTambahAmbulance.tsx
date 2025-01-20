18fee9efbc09eb183f342bae1a9a55b20178aef2
Dimas Purwanto <dimas@jalintraksi.com>
2025-01-20 13:59:21 +0700

    refactor add ambulance

--- src/pages/ambulance/hooks/useTambahAmbulance.tsx
+++ src/pages/ambulance/hooks/useTambahAmbulance.tsx
@@ -19,6 +19,7 @@ type Schedule = {
 }
 
 export default function useTambahAmbulance() {
+  const [currentPage, setCurrentPage] = useState<number>(1);
   const [imagesData, setImagesData] = useState<ImageData[]>([])
   const [errorAlert, setErrorAlert] = useState(false)
   const [selectedDay, setSelectedDay] = useState<string | null>(null)
@@ -141,6 +142,53 @@ export default function useTambahAmbulance() {
     }
   })
 
+  const getPageStyle = (page: number) => {
+    if (page === currentPage) {
+        return { color: "#8F85F3", cursor: "pointer", fontWeight: "bold" };
+    } else if (page < currentPage) {
+        return { color: "#8F85F3", cursor: "pointer" };
+    } else {
+        return { color: "black", cursor: "pointer" };
+    }
+};
+
+const getBorderStyle = (page: number) => {
+    if (page === currentPage) {
+        return {
+            display: "flex",
+            border: "1px solid #8F85F3",
+            width: "38px",
+            height: "38px",
+            borderRadius: "8px",
+            justifyContent: "center",
+            alignItems: "center",
+        };
+    } else if (page < currentPage) {
+        return {
+            display: "flex",
+            border: "1px solid #8F85F3",
+            width: "38px",
+            height: "38px",
+            borderRadius: "8px",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "#8F85F3",
+            color: "white",
+        };
+    } else {
+        return {
+            display: "flex",
+            border: "1px solid #8F85F3",
+            width: "38px",
+            height: "38px",
+            borderRadius: "8px",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "#8F85F3",
+        };
+    }
+};
+
   return {
     errorAlert,
     handleTambahHari,
@@ -153,7 +201,11 @@ export default function useTambahAmbulance() {
     setSelectedDay,
     setStartTime,
     setEndTime,
-    schedules
+    schedules,
+    currentPage,
+    setCurrentPage,
+    getPageStyle,
+    getBorderStyle
   }
 
 }
