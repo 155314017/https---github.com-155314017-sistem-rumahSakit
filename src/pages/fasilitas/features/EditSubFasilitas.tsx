de54c39dac6a7c4acb3642f991dfd96c5db0b494
Jikoyuo <chornaeld@gmail.com>
2025-02-05 15:29:00 +0700

    fixing API

--- src/pages/fasilitas/features/EditSubFasilitas.tsx
+++ src/pages/fasilitas/features/EditSubFasilitas.tsx
@@ -1,156 +1,228 @@
-//component
 import { Container, Box, Typography, Button, FormControl, OutlinedInput } from "@mui/material";
 import BreadCrumbs from "../../../components/medium/BreadCrumbs";
 import bgImage from "../../../assets/img/String.png";
 import AlertSuccess from "../../../components/small/alert/AlertSuccess";
-import CustomTimePicker from "../../../components/small/CustomTimePicker";
 import DropdownListAPI from '../../../components/small/dropdownlist/DropdownListAPI';
 
 //hooks
+import TestKalender from "../../../components/medium/TestKalender";
 import useEditSubFasilitas from "../hooks/useEditSubFasilitas";
 
-
 export default function EditSubFasilitas() {
-    const{
-    breadcrumbItems,
-    formData: formik,
-    successAlert,
-    errorAlert,
-    startTime,
-    endTime,
-    handleTambahHari,
-    selectedDays,
-    showTemporaryAlertSuccess,
-    setSelectedDay,
-    facilityOptions,
-    setEndTime,
-    setStartTime
-    }=useEditSubFasilitas();
-  return (
-    <Container sx={{ py: 2 }}>
-    <BreadCrumbs
-        breadcrumbItems={breadcrumbItems}
-        onBackClick={() => window.history.back()}
-    />
-    <Box mt={3}>
-        <Box position="relative" p={3} sx={{ borderRadius: "24px", bgcolor: "#fff", overflow: "hidden" }}>
-            <Typography fontSize="20px" fontWeight="700">Edit SubFasilitas</Typography>
-            <Box position="absolute" sx={{ top: 0, right: 0 }}>
-                <img src={bgImage} alt="bg-image" />
-            </Box>
+    const {
+        formik,
+        breadcrumbItems,
+        setCurrentPage,
+        getPageStyle,
+        getBorderStyle,
+        currentPage,
+        handleEditFasilitas,
+        id,
+        errorAlert,
+        facilityOptions,
+        kalenderRef,
+        scheduleDataPraktek,
+        scheduleDataPengecualian
+    } = useEditSubFasilitas()
 
-            <Box component="form" noValidate autoComplete="off" mt={3} onSubmit={formik.handleSubmit}>
-                <Typography sx={{ fontSize: "16px" }}>Nama Sub Fasilitas<span style={{ color: "red" }}>*</span></Typography>
-                <FormControl fullWidth sx={{ my: 1 }}>
-                    <OutlinedInput
-                        id="namaSubFasilitas"
-                        name="namaSubFasilitas"
-                        size="small"
-                        placeholder="Masukkan nama fasilitas"
-                        value={formik.values.namaSubFasilitas}
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        error={formik.touched.namaSubFasilitas && Boolean(formik.errors.namaSubFasilitas)}
-                    />
-                    {formik.touched.namaSubFasilitas && formik.errors.namaSubFasilitas && (
-                        <Typography color="error">{formik.errors.namaSubFasilitas}</Typography>
-                    )}
-                </FormControl>
 
-                <Typography sx={{ fontSize: "16px", mt: 2 }}>Pilih Fasilitas<span style={{ color: "red" }}>*</span></Typography>
-                <DropdownListAPI
-                    options={facilityOptions.map(({ id, name }) => ({ value: id, label: name }))}
-                    placeholder="Pilih Fasilitas Induk"
-                    defaultValue={formik.values.masterFacilityId}
-                    onChange={(selectedOptionValue) => {
-                        formik.setFieldValue('masterFacilityId', selectedOptionValue);
-                    }}
-                    loading={false}
-                />
+    return (
+        <Container sx={{ py: 2, minWidth: '1500px' }}>
+            <BreadCrumbs breadcrumbItems={breadcrumbItems} onBackClick={() => window.history.back()} />
 
-                <Box display={'flex'} flexDirection={'column'} border={'1px solid #A8A8BD'} borderRadius={'16px'} padding={'16px'} mt={2}>
-                    <Typography mb={'15px'} >Jam Operasional</Typography>
-                    <Box display={'flex'} flexDirection={'row'} justifyContent={'space-between'} gap={'32px'} >
-                        {/* Hari */}
-                        <Box display={'flex'} flexDirection={'column'} width={'100%'} >
-                            <Typography>Hari</Typography>
-                            <DropdownListAPI
-                                options={[
-                                    { value: "1", label: "Senin" },
-                                    { value: "2", label: "Selasa" },
-                                    { value: "3", label: "Rabu" },
-                                    { value: "4", label: "Kamis" },
-                                    { value: "5", label: "Jumat" },
-                                    { value: "6", label: "Sabtu" },
-                                    { value: "7", label: "Minggu" },
-                                ]}
-                                placeholder="Pilih hari"
-                                onChange={(value: string) => setSelectedDay(value)}
-                                loading={false}
-                                defaultValue={selectedDays}
+            <Box mt={3}>
+                <Box position="relative" p={3} sx={{ borderRadius: "24px", bgcolor: "#FAFAFA", overflow: "hidden" }}>
+                    {/* Bentuk lengkung atas */}
+                    <Box
+                        position={"absolute"}
+                        sx={{
+                            top: 0,
+                            left: "50%",
+                            transform: "translateX(-50%)",
+                            display: "flex",
+                        }}
+                    >
+                        {/* Lengkung kiri */}
+                        <Box
+                            sx={{
+                                width: "50px",
+                                height: "30px",
+                                bgcolor: "#F1F0FE",
+                            }}
+                        >
+                            <Box
+                                sx={{
+                                    width: "50px",
+                                    height: "30px",
+                                    bgcolor: "#FAFAFA",
+                                    borderRadius: "0px 15px 0px 0px ",
+                                }}
                             />
                         </Box>
 
-                        {/* Jam Mulai */}
-                        <Box display={'flex'} flexDirection={'column'} width={'100%'} >
-                            <Typography>Jam mulai</Typography>
-                            <CustomTimePicker
-                                value={startTime}
-                                onChange={(newValue) => setStartTime(newValue)}
-                            />
-                        </Box>
+                        {/* Kotak tengah */}
+                        <Box
+                            sx={{
+                                width: "600px",
+                                height: "50px",
+                                bgcolor: "#F1F0FE",
+                                borderRadius: "0px 0px 22px 22px",
+                            }}
+                        />
 
-                        {/* Jam Selesai */}
-                        <Box display={'flex'} flexDirection={'column'} width={'100%'} >
-                            <Typography>Jam selesai</Typography>
-                            <CustomTimePicker
-                                value={endTime}
-                                onChange={(newValue) => setEndTime(newValue)}
+                        {/* Lengkung kanan */}
+                        <Box
+                            sx={{
+                                width: "50px",
+                                height: "30px",
+                                bgcolor: "#F1F0FE",
+                            }}
+                        >
+                            <Box
+                                sx={{
+                                    width: "50px",
+                                    height: "30px",
+                                    bgcolor: "#FAFAFA",
+                                    borderRadius: "15px 0px 0px 0px ",
+                                }}
                             />
                         </Box>
                     </Box>
-                    <Button
-                        fullWidth
-                        sx={{
-                            mt: 2,
-                            bgcolor: 'transparent',
-                            color: '#8F85F3',
-                            border: '1px solid #8F85F3',
-                            ":hover": { bgcolor: '#8F85F3', color: 'white' },
-                        }}
-                        onClick={handleTambahHari}
-                    >
-                        + Tambah hari
-                    </Button>
-                </Box>
 
-                <Button
-                    type="submit"
-                    onClick={showTemporaryAlertSuccess}
-                    variant="contained"
-                    color="inherit"
-                    sx={{
-                        mt: 3,
-                        width: "100%",
-                        bgcolor: "#8F85F3",
-                        color: "#fff",
-                        textTransform: "none",
-                        borderRadius: "8px",
-                        ":hover": { bgcolor: "#a098f5" },
-                    }}
-                    disabled={!formik.isValid || !formik.dirty}
-                >
-                    Simpan
-                </Button>
+                    <Typography fontSize="20px" fontWeight="700">
+                        Edit Fasilitas
+                    </Typography>
+
+                    <Box
+                        sx={{ display: "flex", flexDirection: "row", mt: 2, mb: 2, justifyContent: 'space-between', ml: 2 }}
+                    >
+                        <Box display={"flex"} flexDirection={"row"} width={"400px"}>
+                            <Box
+                                display={"flex"}
+                                flexDirection={"row"}
+                                alignItems="center"
+                                onClick={() => setCurrentPage(1)}
+                                sx={getPageStyle(1)}
+                                mx={2}
+                            >
+                                <Box sx={getBorderStyle(1)}>1</Box>
+                                <Typography sx={{ ml: 1 }}>
+                                    Informasi Fasilitas
+                                </Typography>
+                            </Box>
+                        </Box>
+
+                        <Box display={"flex"} flexDirection={"row"} width={"800px"}>
+                            <Box
+                                display={"flex"}
+                                flexDirection={"row"}
+                                alignItems="center"
+                                onClick={() => setCurrentPage(2)}
+                                sx={getPageStyle(2)}
+                                mx={2}
+                            >
+                                <Box sx={getBorderStyle(2)}>2</Box>
+                                <Typography sx={{ ml: 1 }}>
+                                    Jam Operasional
+                                </Typography>
+                            </Box>
+                        </Box>
+                    </Box>
+
+                    <Box position="absolute" sx={{ top: 0, right: 0 }}>
+                        <img src={bgImage} alt="bg-image" />
+                    </Box>
+
+                    {currentPage === 1 && (
+                        <>
+                            <Typography fontSize="20px" fontWeight="700" mb="32px" mt="54px">
+                                Informasi SubFasilitas
+                            </Typography>
+                            <Box component="form" noValidate autoComplete="off" mt={3}>
+                                <Typography sx={{ fontSize: "16px" }}>Nama SubFasilitas<span style={{ color: "red" }}>*</span></Typography>
+                                <FormControl fullWidth sx={{ my: 1 }}>
+                                    <OutlinedInput
+                                        id="namaFasilitas"
+                                        name="namaFasilitas"
+                                        size="small"
+                                        placeholder="Masukkan nama fasilitas"
+                                        value={formik.values.namaFasilitas}
+                                        onChange={formik.handleChange}
+                                        onBlur={formik.handleBlur}
+                                        error={formik.touched.namaFasilitas && Boolean(formik.errors.namaFasilitas)}
+                                    />
+                                    {formik.touched.namaFasilitas && formik.errors.namaFasilitas && (
+                                        <Typography color="error">{formik.errors.namaFasilitas}</Typography>
+                                    )}
+                                </FormControl>
+
+                                <Typography sx={{ fontSize: "16px", mt: 2 }}>Pilih Fasilitas<span style={{ color: "red" }}>*</span></Typography>
+                                <DropdownListAPI
+                                    options={facilityOptions.map(({ id, name }) => ({ value: id, label: name }))}
+                                    placeholder="Pilih Fasilitas Induk"
+                                    defaultValue={formik.values.masterFacilityId}
+                                    onChange={(selectedOptionValue) => {
+                                        formik.setFieldValue('masterFacilityId', selectedOptionValue);
+                                    }}
+                                    loading={false}
+                                />
+
+
+                                <Button
+                                    variant="contained"
+                                    color="inherit"
+                                    sx={{
+                                        mt: 8,
+                                        width: "100%",
+                                        bgcolor: "#8F85F3",
+                                        color: "#fff",
+                                        textTransform: "none",
+                                        borderRadius: "8px",
+                                        boxShadow: 'none',
+                                        ":hover": { bgcolor: "#a098f5", boxShadow: 'none' },
+                                    }}
+                                    onClick={() => setCurrentPage(2)}
+                                >
+                                    Selanjutnya
+                                </Button>
+                            </Box>
+                        </>
+                    )}
+
+                    {currentPage === 2 && (
+                        <>
+                            <TestKalender
+                                ref={kalenderRef}
+                                initialData={scheduleDataPraktek}
+                                initialDataPengecualian={scheduleDataPengecualian}
+                                typeId={id}
+                            />
+                            <Button
+                                type="submit"
+                                onClick={handleEditFasilitas}
+                                variant="contained"
+                                color="inherit"
+                                sx={{
+                                    mt: 3,
+                                    width: "100%",
+                                    bgcolor: "#8F85F3",
+                                    color: "#fff",
+                                    textTransform: "none",
+                                    borderRadius: "8px",
+                                    boxShadow: 'none',
+                                    ":hover": { bgcolor: "#a098f5", boxShadow: 'none' },
+                                }}
+                                disabled={!formik.isValid || !formik.dirty}
+                            >
+                                Simpan
+                            </Button>
+                        </>
+                    )}
+                </Box>
             </Box>
-        </Box>
-    </Box>
-    {successAlert && (
-        <AlertSuccess label="Success adding SubFacility" />
-    )}
-    {errorAlert && (
-        <AlertSuccess label="Error adding SubFacility" />
-    )}
-</Container>
-  )
+            {errorAlert && (
+                <AlertSuccess label="Error adding building" />
+            )}
+        </Container>
+    )
 }
--- src/services/ManageFacility/GetSubFacilityByIdServices.ts
+++ src/services/ManageFacility/GetSubFacilityByIdServices.ts
@@ -1,66 +1,51 @@
-import axios from 'axios';
+import axios from 'axios'
+import Cookies from 'js-cookie'
+import { ScheduleData } from '../Admin Tenant/ManageSchedule/GetScheduleById'
 
-// Define the interfaces for the request data structure
-export interface Schedule {
-    startDateTime: string; // ISO 8601 format
-    endDateTime: string;   // ISO 8601 format
-}
-
-export interface Image {
-    imageName: string;
-    imageType: string;
-    imageData: string; // Base64 encoded image
-}
-
-export interface FacilityRequest {
-    name: string;
-    facilityDataId: string;
-    description: string;
-    cost: number;
-    additionalInfo: string;
-    schedules: { startDateTime: number; endDateTime: number }[];
-    images: { imageName: string; imageType: string; imageData: string }[];
+// Interface untuk response Ambulance
+export interface SubFacilityData {
+  id: string
+  name: string
+  description: string
+  additionalInfo: string
+  createdBy: string
+  createdDateTime: number
+  updatedBy: string | null
+  updatedDateTime: number | null
+  deletedBy: string | null
+  deletedDateTime: number | null
+  facilityDataId: string
+  cost: number
+  schedules: ScheduleData[]
 }
 
 export interface ApiResponse {
-    responseCode: string;
-    statusCode: string;
-    message: string;
-    data: FacilityRequest;
+  responseCode: string
+  responseMessage: string
+  statusCode: string
+  data: SubFacilityData
 }
 
+// Layanan untuk mendapatkan data Ambulance
+export const GetSubFacilityByIdService = async (
+  id: string | undefined
+): Promise<SubFacilityData | null> => {
+  try {
+    const token = Cookies.get('accessToken')
+    if (!token) throw new Error('Access token not found')
 
-// Function to create a facility
-export const GetSubFacilityById = async (
-    id: string | undefined,
-    token: string | undefined
-): Promise<FacilityRequest> => {
-    
-    
-     // Assuming 'accessToken' is stored in cookies
-
-    try {
-        // Make the POST request to create the facility
-        const response = await axios.get<ApiResponse>(`${import.meta.env.VITE_APP_BACKEND_URL_BASE}/v1/manage/subfacility/${id}`, {
-            headers: {
-                'Content-Type': 'application/json',
-                'accessToken': `${token}`
-            }
-        });
-
-        // Handle successful response
-        if (response.status === 200) {
-            return response.data.data; // Return the response data
-        } else {
-            throw new Error(`API responded with status: ${response.status}`);
+    const response = await axios.get<ApiResponse>(
+      `${import.meta.env.VITE_APP_BACKEND_URL_BASE}/v1/manage/subfacility/${id}`,
+      {
+        headers: {
+          'Content-Type': 'application/json'
         }
-    } catch (error: unknown) {
-        // Error handling
-        if (axios.isAxiosError(error)) {
-            console.error("Axios error:", error.message);
-        } else {
-            console.error("Unexpected error:", error);
-        }
-        throw error; // Re-throw the error for the caller to handle
-    }
-};
+      }
+    )
+
+    return response.data.data
+  } catch (error) {
+    console.error('Error fetching facility data:', error)
+    return null
+  }
+}
