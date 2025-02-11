ac600e8d68794bfec5d6b9de278469327c6fe6a2
Jikoyuo <chornaeld@gmail.com>
2025-02-11 19:44:42 +0700

    fix bug API patient

--- src/pages/pasien/hooks/useIndex.tsx
+++ src/pages/pasien/hooks/useIndex.tsx
@@ -9,74 +9,74 @@ import { PatientDataItem } from "../../../types/patient.types";
 
 export const PAGE_SIZE = 10;
 export default function useIndex() {
-    const [open, setOpen] = useState<boolean>(false);
-    const [data, setData] = useState<PatientDataItem[]>([]);
-    const [loading, setLoading] = useState<boolean>(false);
-    const [pageNumber, setPageNumber] = useState(0);
-    const [orderBy, setOrderBy] = useState("createdDateTime=asc");
-    const [totalElements, setTotalElements] = useState(0);
-    const [successAddPatient, setSuccessAddPatient] = useState(false);
-    const [successDeletePatient, setSuccessDeletePatient] = useState(false);
-    const [successEditPatient, setSuccessEditPatient] = useState(false);
-    const location = useLocation();
-    const navigate = useNavigate();
+  const [open, setOpen] = useState<boolean>(false);
+  const [data, setData] = useState<PatientDataItem[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [pageNumber, setPageNumber] = useState(0);
+  const [orderBy, setOrderBy] = useState("createdDateTime=asc");
+  const [totalElements, setTotalElements] = useState(0);
+  const [successAddPatient, setSuccessAddPatient] = useState(false);
+  const [successDeletePatient, setSuccessDeletePatient] = useState(false);
+  const [successEditPatient, setSuccessEditPatient] = useState(false);
+  const location = useLocation();
+  const navigate = useNavigate();
 
-    
-        const fetchData = useCallback(async () => {
-            try {
-                setLoading(true)
-                const result = await PatientServices(pageNumber, PAGE_SIZE, orderBy);
-                setTotalElements(result.data.totalElements)
-                setData(result.data.content);
-            } catch (error) {
-                console.error('Failed to fetch data from API' + error);
-            } finally {
-              setLoading(false);
 
-            }
-        
-    }, [pageNumber, orderBy]);
+  const fetchData = useCallback(async () => {
+    try {
+      setLoading(true)
+      const result = await PatientServices(pageNumber, PAGE_SIZE, orderBy);
+      setTotalElements(result.data.totalElements)
+      setData(result.data.content);
+    } catch (error) {
+      console.error('Failed to fetch data from API' + error);
+    } finally {
+      setLoading(false);
 
-    useEffect(() => {
-      fetchData();
-    }, [fetchData]);
+    }
 
-    useEffect(() => {
-        const handleLocationState = async () => {
-          if (location.state) {
-            if (location.state.successAdd) {
-              await showTemporaryAlertSuccess();
-            } else if (location.state.successEdit) {
-              await showTemporarySuccessEdit();
-            } else if (location.state.successDelete) {
-              await showTemporarySuccessDelete();
-            }
-            navigate(location.pathname, { replace: true, state: undefined });
-            fetchData();
-          }
-        };
-    
-        handleLocationState();
-      }, [location.state]);
+  }, [pageNumber, orderBy]);
 
-      const showTemporaryAlertSuccess = async () => {
-        setSuccessAddPatient(true);
-        await new Promise((resolve) => setTimeout(resolve, 3000));
-        setSuccessAddPatient(false);
-      };
-    
-      const showTemporarySuccessDelete = async () => {
-        setSuccessDeletePatient(true);
-        await new Promise((resolve) => setTimeout(resolve, 3000));
-        setSuccessDeletePatient(false);
-      };
-    
-      const showTemporarySuccessEdit = async () => {
-        setSuccessEditPatient(true);
-        await new Promise((resolve) => setTimeout(resolve, 3000));
-        setSuccessEditPatient(false);
-      };
-      
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  useEffect(() => {
+    const handleLocationState = async () => {
+      if (location.state) {
+        if (location.state.successAdd) {
+          await showTemporaryAlertSuccess();
+        } else if (location.state.successEdit) {
+          await showTemporarySuccessEdit();
+        } else if (location.state.successDelete) {
+          await showTemporarySuccessDelete();
+        }
+        navigate(location.pathname, { replace: true, state: undefined });
+        fetchData();
+      }
+    };
+
+    handleLocationState();
+  }, [location.state]);
+
+  const showTemporaryAlertSuccess = async () => {
+    setSuccessAddPatient(true);
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    setSuccessAddPatient(false);
+  };
+
+  const showTemporarySuccessDelete = async () => {
+    setSuccessDeletePatient(true);
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    setSuccessDeletePatient(false);
+  };
+
+  const showTemporarySuccessEdit = async () => {
+    setSuccessEditPatient(true);
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    setSuccessEditPatient(false);
+  };
+
   return {
     data,
     open,
@@ -92,6 +92,6 @@ export default function useIndex() {
     totalElements,
     setTotalElements,
     showTemporarySuccessDelete
-    
+
   }
 }
--- src/services/ManagePatient/PatientServices.ts
+++ src/services/ManagePatient/PatientServices.ts
@@ -1,12 +1,12 @@
 import axios from 'axios'
-import { PaginatedResponse } from '../../types/api.types';
-import { PatientDataItem } from '../../types/patient.types';
+import { PaginatedResponse } from '../../types/api.types'
+import { PatientDataItem } from '../../types/patient.types'
 
 // Interface untuk properti dalam response API
 
-
-const API_URL = `${import.meta.env.VITE_APP_BACKEND_URL_BASE}/v1/manage/patient/?pageNumber=0&pageSize=10&orderBy=id=asc`;
-
+const API_URL = `${
+  import.meta.env.VITE_APP_BACKEND_URL_BASE
+}/v1/manage/patient/?`
 
 export const PatientServices = async (
   pageNumber: number = 0,
@@ -14,28 +14,22 @@ export const PatientServices = async (
   orderBy: string = 'createdDateTime=asc'
 ): Promise<PaginatedResponse<PatientDataItem>> => {
   try {
-    const response = await axios.get<PaginatedResponse<PatientDataItem>>(API_URL,{
+    const response = await axios.get<PaginatedResponse<PatientDataItem>>(API_URL, {
       params: {
         pageNumber,
         pageSize,
         orderBy
       }
-    }
-    )
+    })
 
     if (response.status === 200) {
       const patients = response.data
-
-      
-
-      
-
-      return patients // Kembalikan semua data pasien
+      return patients 
     } else {
       throw new Error(`API responded with status: ${response.status}`)
     }
   } catch (error) {
     console.error('Error fetching patient services:', error)
-    throw error // Re-throw untuk penanganan oleh pemanggil fungsi
+    throw error 
   }
 }
